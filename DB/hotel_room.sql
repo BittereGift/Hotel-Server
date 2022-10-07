f56a645034983d1bae5dbfacf10093d18a30cf0b
Bittere_Gift <329056266@qq.com>
2022-10-07 23:35:38 +0800

    add features

--- DB/hotel_room.sql
+++ DB/hotel_room.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 05/10/2022 01:22:08
+ Date: 07/10/2022 23:35:01
 */
 
 SET NAMES utf8mb4;
@@ -26,19 +26,36 @@ CREATE TABLE `room`  (
   `hotel_id` int NULL DEFAULT NULL,
   `type_id` int NULL DEFAULT NULL,
   `position` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of room
 -- ----------------------------
-INSERT INTO `room` VALUES (1, 1, 1, '101', '0');
-INSERT INTO `room` VALUES (2, 1, 1, '102', '0');
-INSERT INTO `room` VALUES (3, 2, 1, '101', '0');
-INSERT INTO `room` VALUES (4, 2, 2, '102', '0');
-INSERT INTO `room` VALUES (5, 3, 2, '101', '0');
-INSERT INTO `room` VALUES (6, 3, 1, '102', '0');
+INSERT INTO `room` VALUES (1, 1, 1, '101');
+INSERT INTO `room` VALUES (2, 1, 1, '102');
+INSERT INTO `room` VALUES (3, 2, 1, '101');
+INSERT INTO `room` VALUES (4, 2, 2, '102');
+INSERT INTO `room` VALUES (5, 3, 2, '101');
+INSERT INTO `room` VALUES (6, 3, 1, '102');
+
+-- ----------------------------
+-- Table structure for room_book_time
+-- ----------------------------
+DROP TABLE IF EXISTS `room_book_time`;
+CREATE TABLE `room_book_time`  (
+  `order_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of room_book_time
+-- ----------------------------
+INSERT INTO `room_book_time` VALUES (1, 1, '2022-10-05 00:00:00', '2022-10-08 00:00:00');
+INSERT INTO `room_book_time` VALUES (25, 2, '2022-11-01 08:00:00', '2022-11-06 08:00:00');
 
 -- ----------------------------
 -- Table structure for room_type
@@ -49,7 +66,7 @@ CREATE TABLE `room_type`  (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `price` decimal(10, 2) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of room_type
